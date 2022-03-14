389b7c39dba58a4c171ed0eaba0a81abe4208dc8
divyapujar11 <99553089+divyapujar11@users.noreply.github.com>
2022-03-14 13:11:34 +0530

    Add files via upload

---
+++ JAVA/Jacoco/src/main/java/compare/compareString.java
@@ -0,0 +1,10 @@
+package compare;
+
+public class compareString {
+
+	public void containsOrnot(String value) {
+		if(value.contains("Automation")) {
+			System.out.println("We are softer tester");
+		}
+	}
+}
