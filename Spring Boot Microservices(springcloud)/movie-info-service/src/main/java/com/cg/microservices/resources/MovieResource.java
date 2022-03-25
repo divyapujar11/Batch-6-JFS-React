54bca7659507450a8be78c7cebc914c77167a13a
divyapujar11 <99553089+divyapujar11@users.noreply.github.com>
2022-03-25 11:30:39 +0530

    hands-on communication and service discovery

---
+++ Spring Boot Microservices(springcloud)/movie-info-service/src/main/java/com/cg/microservices/resources/MovieResource.java
@@ -0,0 +1,18 @@
+package com.cg.microservices.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.microservices.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		return new Movie(movieId, "Test name");
+	}
+
+}
